aff8f4203718372c28822f289667d31ea39382bd
Diego Andres Benitez <47540258+andres-1994@users.noreply.github.com>
2020-07-06 17:28:56 -0400

    v1

---
+++ javascript/script.js
@@ -0,0 +1,17 @@
+var menu = document.getElementById('menu');
+
+var altura = menu.offsetTop;
+
+window.addEventListener('scroll', function (){
+	
+	if (window.pageYOffset > altura){
+
+		menu.classList.add('fixed');
+
+	}else{
+
+		menu.classList.remove('fixed');
+	}
+})
+
+
